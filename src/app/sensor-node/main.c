fc4c19b168fe43e43ba528dff6a09b0eaa1b5523
Muhammad Yunus <muhammadyunusiot@gmail.com>
2026-08-19 20:59:41 +0700

    fix: LDR light level send as percentage 0-100 instead of raw ADC (prevents int16 overflow)

--- src/app/sensor-node/main.c
+++ src/app/sensor-node/main.c
@@ -457,8 +457,8 @@ static void PrepareTxFrame( void )
     CayenneLppAddDigitalInput( channel++, AppLedStateOn );
     CayenneLppAddAnalogInput(  channel++, BoardGetBatteryLevel( ) * 100 / 254 );
     CayenneLppAddTemperature(  channel++, ( float )BoardReadNtcTemperatureX10( ) / 10.0f );
-    /* Send raw ADC value as analog input (12-bit, 0-4095) */
-    CayenneLppAddAnalogInput( channel++, ( float )BoardReadLdrRawAdc() );
+    /* Send light level as analog input (0-100 percent) */
+    CayenneLppAddAnalogInput( channel++, ( float )BoardReadLdrLightLevel() );
     /* Send PIR motion status as presence */
     CayenneLppAddPresence( channel++, BoardReadPirMotion() );
     /* Read LDR multiple times for debug */
