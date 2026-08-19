f276fa55bb88b41b2e55863e177146ab052bb1a5
Muhammad Yunus <muhammadyunusiot@gmail.com>
2026-08-19 21:27:46 +0700

    fix: use raw ADC LDR with LPP_ANALOG_INPUT

--- src/app/sensor-node/main.c
+++ src/app/sensor-node/main.c
@@ -457,8 +457,8 @@ static void PrepareTxFrame( void )
     CayenneLppAddDigitalInput( channel++, AppLedStateOn );
     CayenneLppAddAnalogInput(  channel++, BoardGetBatteryLevel( ) * 100 / 254 );
     CayenneLppAddTemperature(  channel++, ( float )BoardReadNtcTemperatureX10( ) / 10.0f );
-    /* Send raw ADC as luminosity (lux) */
-    CayenneLppAddLuminosity( channel++, BoardReadLdrRawAdc() );
+    /* Send raw ADC as analog input */
+    CayenneLppAddAnalogInput( channel++, ( float )BoardReadLdrRawAdc() );
     /* Send PIR motion status as presence */
     CayenneLppAddPresence( channel++, BoardReadPirMotion() );
     /* Read LDR multiple times for debug */
